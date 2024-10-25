43a3f70fc79dc01bbf7a11e6e0def1c80f42a07c
Kyle-the-coder <kylethacoder@gmail.com>
2024-10-25 10:42:47 -0600

    updated

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,23 +1,41 @@
+import { useState } from "react";
 import "./projectpage.css";
 
 function ProjectPage() {
-  const projectList = [
+  const [projectIndex, setProjectIndex] = useState(0);
+
+  const projectNameList = [
     "Project 1",
     "Project 2",
     "Project 3",
     "Project 4",
     "Project 5",
   ];
+
+  const projectDisplayList = [
+    "Project 1",
+    "Project 2",
+    "Project 3",
+    "Project 4",
+    "Project 5",
+  ];
+
+  console.log(projectIndex);
+
   return (
     <div className="project-main-container">
       <div className="project-list-container">
-        {projectList.map((item, index) => {
-          return <h1 key={item}>{item}</h1>;
+        {projectNameList.map((item, index) => {
+          return (
+            <h1 key={item} onClick={() => setProjectIndex(index)}>
+              {item}
+            </h1>
+          );
         })}
       </div>
       <div className="project-display-container">
         <div className="project-container">
-          <h1>This is a project </h1>
+          {projectDisplayList.filter((i, index) => index === projectIndex)}
         </div>
       </div>
     </div>
