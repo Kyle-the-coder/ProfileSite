52183a5b8821200a25867d92261a003a5062393b
Kyle-the-coder <kylethacoder@gmail.com>
2024-10-25 10:24:35 -0600

    updated

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,7 +1,15 @@
+import "./projectpage.css";
+
 function ProjectPage() {
   return (
     <div className="project-main-container">
-      <div className="project-list-container"></div>
+      <div className="project-list-container">
+        <h1>Project 1</h1>
+        <h1>Project 2</h1>
+        <h1>Project 3</h1>
+        <h1>Project 4</h1>
+        <h1>Project 5</h1>
+      </div>
       <div className="project-display-container"></div>
     </div>
   );
