7af27c573353ca1614f0aba0b1bcd7058ae59b2e
Kyle-the-coder <kylethacoder@gmail.com>
2024-10-28 13:59:30 -0600

    updated

--- src/components/ProjectDisplay/ProjectDisplay.jsx
+++ src/components/ProjectDisplay/ProjectDisplay.jsx
@@ -25,6 +25,7 @@ export function ProjectDisplay({ video, title }) {
             </p>
           </div>
         </div>
+
         <div className="project-vid-exp">
           <div className="project-vid-details">
             <h1>Title</h1>
@@ -37,6 +38,20 @@ export function ProjectDisplay({ video, title }) {
             </p>
           </div>
           <video src={video} controls />
+        </div>
+      </div>
+
+      <div className="project-vid-quote">
+        <div className="project-vid-details">
+          <h1>Conclusion</h1>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris. Nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum.
+          </p>
         </div>
       </div>
     </div>
