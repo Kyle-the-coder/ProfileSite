c6e90cb10a286afa4e36480c6b73ce265de4a214
Kyle-the-coder <kylethacoder@gmail.com>
2024-10-30 11:26:12 -0600

    updated

--- src/components/ImgGrid/ImgGrid.jsx
+++ src/components/ImgGrid/ImgGrid.jsx
@@ -1,93 +1,22 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
-import gsap from "gsap";
 import "./imggrid.css";
 import rjp from "../../assets/rjp.png";
 import rms from "../../assets/rms.png";
 import poke from "../../assets/poke.png";
 import apc from "../../assets/apc.png";
-import Loader from "../Loader/Loader";
-import { useInView } from "react-intersection-observer";
+import cal from "../../assets/cal.png";
 
-const ImgGrid = ({ setIsFirstRender }) => {
-  const [images1, setImages1] = useState([rjp, rms]);
-  const [images2, setImages2] = useState([apc]);
-  const [images3, setImages3] = useState([poke]);
-  const [loading, setLoading] = useState(false);
-  const imgArr1 = ["Sapphire Ring", "Emerald Ring"];
-  const imgArr2 = ["Diamond Earrings"];
-  const imgArr3 = ["Wedding", "Engagement"];
-  const [ref, inView] = useInView({});
-
-  function findArr(arr) {
-    const ranIndex = Math.floor(Math.random() * arr.length);
-    return arr[ranIndex];
-  }
-
-  useEffect(() => {
-    if (inView) {
-      gsap.to(".img-grid-display", {
-        duration: 75.2,
-        yPercent: -100,
-
-        delay: 0.5,
-      });
-      gsap.to(".img-grid-display-reverse", {
-        duration: 75.2,
-        yPercent: 80,
-        ease: "slow(0.3,0.4,false)",
-        delay: 0.5,
-      });
-    }
-  }, [inView]);
-
-  console.log(images1);
-
+const ImgGrid = ({}) => {
   return (
-    <div className="img-grid-container" ref={ref}>
-      <div className="img-grid">
-        {loading ? (
-          <div className="spinner-container">
-            {/* Create 18 card elements */}
-            {Array.from({ length: 5 }, (_, index) => (
-              <div key={index} className="spinner-card">
-                <Loader />
-              </div>
-            ))}
-          </div>
-        ) : (
-          <>
-            <div className="img-grid-display">
-              <img
-                id="forward"
-                src={rjp}
-                alt={"picture"}
-                className="img-display-img"
-              />
-              <img
-                id="forward"
-                src={poke}
-                alt={"picture"}
-                className="img-display-img"
-              />
-            </div>
-            <div className="img-grid-display-reverse">
-              <img
-                id="reverse"
-                src={apc}
-                alt={"picture"}
-                className="img-display-img"
-              />
-            </div>
-            <div className="img-grid-display">
-              <img
-                id="forward"
-                src={rms}
-                alt={"picture"}
-                className="img-display-img"
-              />
-            </div>
-          </>
-        )}{" "}
+    <div className="img-grid-container-main">
+      <div className="grid-1">
+        <img src={rjp} className="grid-img" />
+        <img src={rms} className="grid-img" />
+      </div>
+
+      <div className="grid-2">
+        <img src={poke} className="grid-img" />
+        <img src={apc} className="grid-img" />
+        <img src={cal} className="grid-img" />
       </div>
     </div>
   );
