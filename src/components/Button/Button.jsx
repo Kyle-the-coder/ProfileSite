2f6e9bf78f6fea3abed6c15c40f6526731c016e4
Kyle-the-coder <kylethacoder@gmail.com>
2024-12-10 14:49:16 -0700

    updated

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import "./button.css";
 
-export function Button({ buttonName, size, margin, padding }) {
+export function Button({ buttonName, size, margin, padding, nav }) {
+  const navigate = useNavigate();
   return (
     /* From Uiverse.io by Codecite */
     <button
+      onClick={() => {
+        nav && navigate(nav);
+      }}
       className="btn"
       style={{ fontSize: size, margin: margin, padding: padding }}
     >
