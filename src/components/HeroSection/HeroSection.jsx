bca7c1dab6aacbfc053ffe1069bd14efd2fd29f5
Kyle-the-coder <kylethacoder@gmail.com>
2024-12-10 14:49:21 -0700

    updated

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,14 +1,17 @@
 import { Button } from "../Button/Button";
 import herobg from "../../assets/herobg.jpg";
 import "./herosection.css";
+import { useNavigate } from "react-router-dom";
 
 export function HeroSection() {
+  const navigate = useNavigate();
   return (
     <div className="hero-main-container">
       <div className="silhoutte"></div>
       <img src={herobg} className="hero-bg-img" />
       <h1>Organic Online Growth</h1>
       <Button
+        nav="/projects"
         padding="15px 25px 15px 25px"
         buttonName={"See Projects"}
         size="1.5rem"
