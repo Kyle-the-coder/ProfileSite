dccf7fa327ff38d50f72289075ea66aaff7ed14d
Kyle-the-coder <kylethacoder@gmail.com>
2024-12-12 11:41:17 -0700

    updated

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,7 +1,9 @@
-export function Quote(quote) {
+import "./quote.css";
+
+export function Quote({ children }) {
   return (
     <div className="quote-main-container">
-      <h1>{quote}</h1>
+      <h1>{children}</h1>
     </div>
   );
 }
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,15 @@
 import { HeroSection } from "../../components/HeroSection/HeroSection";
+import { Quote } from "../../components/Quote/Quote";
 
 function LandingPage() {
   return (
     <div>
       <HeroSection />
+      <Quote>
+        <span className="blue-font">Affordable</span> Websites for{" "}
+        <span className="blue-font">New</span> and{" "}
+        <span className="blue-font">Upcoming</span> Businesses
+      </Quote>
     </div>
   );
 }
